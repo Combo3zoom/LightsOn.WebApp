0da28a9d66ab35a803219c8f9579567a4f8a0ad1
Julian Zaiats <zaiatsjulian@gmail.com>
2024-05-15 13:22:41 +0300

    remove and update components, set up maps

--- LightsOn.WebApp/Views/Components/Service/Service.razor.cs
+++ LightsOn.WebApp/Views/Components/Service/Service.razor.cs
@@ -11,49 +11,13 @@ public partial class Service
 {
     protected override async Task OnInitializedAsync()
     {
-        var serviceMenuItems1 = ImmutableList.Create(
-            items: new[]
-            {
-                new ServiceItem("Підбір дизельного генератора",
-                    "- Визначення необхідної потужності ДГУ.\n" +
-                                                              "- Вибір ДГУ з врахувань побажань замовника.\n" +
-                                                              "- Пропозиція по можливих варіантах ДГУ.",
-                    "За домовленістю"),
-                new ServiceItem("Встановлення дизельного генератора",
-                    "- Виїзд на місце інсталяції ДГУ для попереднього огляду.\n" +
-                                                   "- Надання рекомендацій та генерація попередньої пропозиції (кошторису).\n" +
-                                                   "- Підбір постачальників та узгодження з замовником вартості та термінів робіт.\n" +
-                                                   "- Проведення монтажних та пуско-налагоджувальних робіт (протягнення кабелів, програмування генератора) на об'єкті.\n" +
-                                                   "- Тестування обладнання та надання інформації замовником.",
-                    "За домовленістю"),
-                new ServiceItem("Обслуговування енергетичного обладнання замовника",
-                    "- Аналіз енергетичного обладнання (АВР, щити, автомати) та документація об'єкта.\n" +
-                    "- Надання рекомендацій по обслуговуванню та модифікації енергетичного обладнання.\n" +
-                    "- Узгодження вартості і терміну робіт.\n" +
-                    "- Проведення робіт\n" +
-                    "- Формування вихідної документації.",
-                    "За домовленістю"),
-                
-                new ServiceItem("Діагностика енергетичного обладнання",
-                    "- Аналіз енергетичного обладнання (АВР, щити, автомати) та документація об'єкта.\n" +
-                    "- Надання рекомендацій по обслуговуванню та модифікації енергетичного обладнання.\n" +
-                    "- Формування вихідної документації.",
-                    "У межах Львова: 2000 грн + транспортні витрати\n" +
-                    " За межами Львова: по домовленості"),
-            });
-        
-        // var serviceDescriptions = ApiServiceDescription?.GetServiceDescriptionsAsync().Result;
-        //
-        // var serviceMenuItems = ImmutableList.Create(
-        //     items: serviceDescriptions?.ToArray());
-        
         var serviceDescriptions =  await ApiServiceDescription?.GetServiceDescriptionsAsync();
         if (serviceDescriptions != null)
         {
-            var phoneNumberItems = serviceDescriptions
+            var servicesItems = serviceDescriptions
                 .Select(pn => new ServiceItem(pn.HeaderText, pn.MainText, pn.LowerPriceLimit))
                 .ToImmutableList();
-            ServiceMenu = new ServiceMenu(phoneNumberItems);
+            ServiceMenu = new ServiceMenu(servicesItems);
         }
     }
     
--- LightsOn.WebApp/Views/Components/WorkingHoursMap/WorkingHoursMap.razor.cs
+++ LightsOn.WebApp/Views/Components/WorkingHoursMap/WorkingHoursMap.razor.cs
@@ -1,9 +1,67 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using System.Collections.Immutable;
+using LightsOn.WebApp.HttpClients.ApiHttpClient;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
 
 namespace LightsOn.WebApp.Views.Components.WorkingHoursMap;
 
+public sealed record PhoneNumberMenu(
+    ImmutableList<PhoneNumberMenuItem> PhoneNumberItems);
+public sealed record PhoneNumberMenuItem(string PhoneNumber);
+
+public sealed record StreetMenu(
+    ImmutableList<StreetMenuItem> StreetMenuItems);
+public sealed record StreetMenuItem(string Street);
+
+public class City
+{
+    public double Latitude { get; set; }
+    public double Longitude { get; set; }
+    public string Name { get; set; }
+}
 public partial class WorkingHoursMap  : ComponentBase
 {
+    protected override async Task OnInitializedAsync()
+    {
+        var phoneNumbers =  await ApiHttpCompanyPhoneNumber?.GetCompanyPhoneNumbersAsync();
+        if (phoneNumbers != null)
+        {
+            var phoneNumberItems = phoneNumbers
+                .Select(pn => new PhoneNumberMenuItem(FormatPhoneNumber(pn.PhoneNumber)))
+                .ToImmutableList();
+            PhoneNumberMenu = new PhoneNumberMenu(phoneNumberItems);
+        }
+        
+        var streetItems = ImmutableList.Create(
+            items: new[]
+            {
+                new StreetMenuItem("вул. Ярослава Мудрого 24"),
+            });
+        
+        StreetMenu = new StreetMenu(streetItems);
+    }
+    
+    private string FormatPhoneNumber(string rawNumber)
+    {
+        if (rawNumber.StartsWith("+380"))
+            return $"{rawNumber.Substring(0, 4)} {rawNumber.Substring(4, 2)}" +
+                   $" {rawNumber.Substring(6, 3)} {rawNumber.Substring(9, 2)}" +
+                   $" {rawNumber.Substring(11, 2)}";
+        return rawNumber;
+    }
+    
+    private List<City> Cities = new List<City> {
+        new City { Latitude = 49.8075994453431, Longitude = 23.905400226487536,  Name="LGenera" },
+    };
+    
+    public PhoneNumberMenu PhoneNumberMenu { get; set; }
+    public StreetMenu StreetMenu { get; set; }
+    
+    [Inject]
+    private IApiHttpClient ApiHttpCompanyPhoneNumber { get; set; }
+    
+
+    
     [Parameter(CaptureUnmatchedValues = true)]
     public Dictionary<string, object> AdditionalAttributes { get; set; }
 }
